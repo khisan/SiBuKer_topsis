dd234c1fe35a3541d913defd0d4fdcb390165512
khisan <khisan8@gmail.com>
2020-11-11 10:00:10 +0700

    buat rules sendiri untuk daftar alumni dan tambah sweetalert dan alert div bootstrap

--- app/Config/CustomRules.php
+++ app/Config/CustomRules.php
@@ -11,31 +11,31 @@ class CustomRules
   public function nimCheck(string $str, string &$error = null): bool
   {
     $cek = substr($str, -5, 2);
-    if ($str == "18") {
+    if ($cek == "18") {
       return true;
-    } elseif ($str == "51") {
+    } elseif ($cek == "51") {
       return true;
-    } elseif ($str == "11") {
+    } elseif ($cek == "11") {
       return true;
-    } elseif ($str == "53") {
+    } elseif ($cek == "53") {
       return true;
-    } elseif ($str == "13") {
+    } elseif ($cek == "13") {
       return true;
-    } elseif ($str == "51") {
+    } elseif ($cek == "51") {
       return true;
-    } elseif ($str == "12") {
+    } elseif ($cek == "12") {
       return true;
-    } elseif ($str == "14") {
+    } elseif ($cek == "14") {
       return true;
-    } elseif ($str == "21") {
+    } elseif ($cek == "21") {
       return true;
-    } elseif ($str == "26") {
+    } elseif ($cek == "26") {
       return true;
-    } elseif ($str == "24") {
+    } elseif ($cek == "24") {
       return true;
-    } elseif ($str == "25") {
+    } elseif ($cek == "25") {
       return true;
-    } elseif ($str == "22") {
+    } elseif ($cek == "22") {
       return true;
     } else {
       return false;
